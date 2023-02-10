d37698a8aa54c2f8d87a4905a17fb0d136f26400
Janak Mistry <janak.m@simformsolutions.com>
2023-02-10 16:18:46 +0530

    Initial Commit

---
+++ android/app/src/main/java/com/example/jingen_batteryutils_demo/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.jingen_batteryutils_demo;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:jingen_batteryutils_demo/battery_utils.dart';
+import 'package:jni/jni.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _batteryLevel = 0;
+
+  JObject activity = JObject.fromRef(Jni.getCurrentActivity());
+
+  void _getBatteryLevel() {
+    setState(() {
+      _batteryLevel = BatteryUtils(activity).getBatteryStatus();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Your battery level is',
+            ),
+            Text(
+              '$_batteryLevel',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getBatteryLevel,
+        tooltip: 'Get Battery level',
+        child: const Icon(Icons.battery_unknown),
+      ),
+    );
+  }
+}
